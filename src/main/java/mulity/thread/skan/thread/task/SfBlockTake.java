b34768105203d989d4f621759b6f20ecba569ac7
ansangkil <tkdrlf83>
2015-03-13 10:07:11 +0900

    파일 머

--- src/main/java/mulity/thread/skan/thread/task/SfBlockTake.java
+++ src/main/java/mulity/thread/skan/thread/task/SfBlockTake.java
@@ -1,7 +1,6 @@
 
 package mulity.thread.skan.thread.task;
 
-import java.util.Iterator;
 import java.util.concurrent.BlockingQueue;
 
 import mulity.thread.skan.model.User;
@@ -13,7 +12,7 @@ import mulity.thread.skan.utils.StateUtils;
  * Class Name  : SfExecuter.java
  * Description : 
  * Modification Information
- *
+ * 
  *    수정일　　　 　　  수정자　　　     수정내용
  *    ────────────   ─────────   ───────────────────────────────
  *    2014. 12. 17.        ask               최초생성
@@ -21,17 +20,16 @@ import mulity.thread.skan.utils.StateUtils;
  *
  * @author ask
  * @since 2014. 12. 17.
- * @version 
+ * @version
  *
- * Copyright (C) 2014 by SKAN.COMPANY All right reserved.
+ *          Copyright (C) 2014 by SKAN.COMPANY All right reserved.
  */
 public class SfBlockTake<T> implements Runnable {
-	
-	
+
 	private BlockingQueue<T> queue;
-	
-	
+
 	private static int count = 0;
+
 	/**
 	 * 
 	 */
@@ -46,53 +44,44 @@ public class SfBlockTake<T> implements Runnable {
 			while (!queue.isEmpty()) {
 				T t = queue.take();
 				if (t instanceof User) {
-					user = (User)t;
+					user = (User) t;
 				}
-				
-				//System.out.println("take item : " + queue.take() );
+
 				for (int i = 0; i < 1; i++) {
-					//Thread.sleep(2000);
-					Thread.sleep(2000);
-					//System.out.println(Thread.currentThread().getName()+ "-[" + count+"]===== TEST : " + i  + " [size = "+ queue.size()+"]");
+					Thread.sleep(5000);
+					// System.out.println(Thread.currentThread().getName()+ "-[" + count+"]===== TEST : " + i + " [size = "+ queue.size()+"]");
 				}
-				//System.out.println( "Thread " + Thread.currentThread().getName() + " Start" );
-				System.out.println("take item : " + t.toString());
-				//System.out.println( "Thread " + Thread.currentThread().getName() + " end" );
-				count++ ;
-				
+				System.out.println("Thread " + Thread.currentThread().getName()	+ " Start");
+				System.out.println(user);
+				count++;
+
 				StateUtils.getInstance().removeItem(user.getKey());
-				//System.out.println("TTT StateUtils.getInstance().getItemSize() = " + StateUtils.getInstance().getItemSize());
-				//System.out.println("Task Thread Name = (" + Thread.currentThread().getName());
-				
-				// TODO :: 엔진 호출 구현~~
-				
+
 			}
 
 		} catch (Exception e) {
-			
 			e.printStackTrace();
 			Thread.currentThread().interrupt();
 		} finally {
-			isShutdownNow();
+			//isShutdownNow();
 		}
-		
+
 	}
-	
+
+	@Deprecated
 	public boolean isShutdownNow() {
 		boolean isShutdownNow = false;
 		while (!isShutdownNow) {
-			if(StateUtils.getInstance().getItemSize() == 0 ) {
+			if (StateUtils.getInstance().getItemSize() == 0) {
 				isShutdownNow = true;
-				//int activeCount = threadPoolExecutor.getActiveCount();
-				//int queueCount = threadPoolExecutor.getQueue().size();
-				//int waitSeconds = (activeCount + queueCount) * 60 + 60;
-				//System.out.println(waitSeconds);
+				// int activeCount = threadPoolExecutor.getActiveCount();
+				// int queueCount = threadPoolExecutor.getQueue().size();
+				// int waitSeconds = (activeCount + queueCount) * 60 + 60;
+				// System.out.println(waitSeconds);
 				SfBlockRuner.getInstance().shutdown();
 			}
 		}
 		return isShutdownNow;
 	}
-	
 
-	
 }
